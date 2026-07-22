9bb3d1e7a89ee5b41f38cae41d5a8350a95b7061
Lia <lia.erisson@mail.mcgill.ca>
2026-07-22 22:39:06 +0300

    Inserted sample text files for later testing. Created initial Document class.

---
+++ src/main/java/io/github/liaerisson/document/Document.java
@@ -0,0 +1,33 @@
+package io.github.liaerisson.document;
+
+import java.nio.file.Path;
+
+public class Document {
+    private final int id;
+    private final String title;
+    private final Path filePath;
+    private final String content;
+
+    public Document(int id, String title, Path filePath, String content) {
+        this.id = id;
+        this.title = title;
+        this.filePath = filePath;
+        this.content = content;
+    }
+
+    public int getId() {
+        return this.id;
+    }
+
+    public String getTitle() {
+        return this.title;
+    }
+
+    public Path getFilePath() {
+        return this.filePath;
+    }
+
+    public String getContent() {
+        return this.content;
+    }
+}
