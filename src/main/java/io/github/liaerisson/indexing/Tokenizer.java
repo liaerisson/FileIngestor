9ea4e3430b9c5388b6d2200de831c78d7144e073
Lia <lia.erisson@mail.mcgill.ca>
2026-07-24 18:47:45 +0300

    Fixed regex in Tokenizer.java. Created initial tests in TokenizerTest.java. Updated notes.

--- src/main/java/io/github/liaerisson/indexing/Tokenizer.java
+++ src/main/java/io/github/liaerisson/indexing/Tokenizer.java
@@ -7,23 +7,8 @@ public class Tokenizer {
             return null;
         }
         content = content.toLowerCase().replaceAll("[^a-z0-9\\s]", "");
-        String[] words = content.split(" ");
+        String[] words = content.split(" +");
 
         return words;
-    }
-    public static void main(String[] args) {
-
-        String practice = "A prActice ...String ##to toKenize!";
-        String lowerCase = practice.toLowerCase();
-        System.out.println(lowerCase);
-        String improvedPractice = lowerCase.replaceAll("[^a-z0-9\\s]", "");
-        System.out.println(improvedPractice);
-
-        //requires a delimiter --> where to split the string up (every space)
-        String delimiter = " ";
-        String[] words = improvedPractice.split(delimiter); //split is a Java library method
-        for(String word : words) {
-            System.out.println(word);
-        }
     }
 }
---
+++ src/test/java/searchengine/TokenizerTest.java
@@ -0,0 +1,39 @@
+package searchengine;
+import static org.junit.jupiter.api.Assertions.*;
+
+import io.github.liaerisson.indexing.Tokenizer;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class TokenizerTest {
+    private Tokenizer tokenizer;
+
+    @BeforeEach
+    void setUp() {
+        tokenizer = new Tokenizer();
+    }
+
+
+    @Test
+    void convertsTextToLowerCase() {
+        String[] result = tokenizer.tokenize("HELLO WORLD");
+
+        assertArrayEquals(new String[]{"hello", "world"}, result);
+    }
+
+    @Test
+    void removesAllSpaces() {
+        String[] result = tokenizer.tokenize("hello      world    !");
+
+        assertArrayEquals(new String[]{"hello", "world"}, result);
+    }
+
+    /* Future test, ideally
+    @Test
+    void splitsDashedWords() {
+        String[] result = tokenizer.tokenize("hello-world! hello—world! hello–world!");
+
+        assertArrayEquals(new String[]{"hello", "world"}, result);
+    }
+     */
+}
