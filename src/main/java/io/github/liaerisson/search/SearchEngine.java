4ea01900dabbf6bb32b42bb54fd5ee8e34ea7f75
Lia <lia.erisson@mail.mcgill.ca>
2026-07-31 16:24:35 -0400

    Create SearchEngine.java class and unit test for usage of functionalities.

--- src/main/java/io/github/liaerisson/search/SearchEngine.java
+++ src/main/java/io/github/liaerisson/search/SearchEngine.java
@@ -1,4 +1,30 @@
 package io.github.liaerisson.search;
 
+import io.github.liaerisson.document.Document;
+import io.github.liaerisson.indexing.InvertedIndex;
+
+import java.util.List;
+
 public class SearchEngine {
+    private final InvertedIndex index;
+    private final QueryProcessor queryProcessor;
+
+    public SearchEngine() {
+        index = new InvertedIndex();
+        queryProcessor = new QueryProcessor(index);
+    }
+
+    public void addDocument(Document document) {
+        index.addDocument(document);
+    }
+
+    public List<SearchResult> search(String query) {
+        return queryProcessor.processQuery(query);
+    }
+
+    public void addDocuments(List<Document> documents) {
+        for(Document document:documents) {
+            index.addDocument(document);
+        }
+    }
 }
---
+++ src/test/java/searchengine/SearchEngineTest.java
@@ -0,0 +1,50 @@
+package searchengine;
+
+import io.github.liaerisson.document.Document;
+import io.github.liaerisson.search.QueryProcessor;
+import io.github.liaerisson.search.SearchEngine;
+import io.github.liaerisson.search.SearchResult;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertSame;
+
+public class SearchEngineTest {
+    SearchEngine engine;
+
+    @BeforeEach
+    void setUp() {
+        engine = new SearchEngine();
+    }
+
+    @Test
+    void getsCorrectResultsAndOrder() {
+        Document docOne = new Document(1, "File One", Path.of("file-one.txt"), "Contents of file one are of little importance");
+        Document docTwo = new Document(2, "File Two", Path.of("file-two.txt"), "Of course file two contains no mention of java...oops!");
+        Document docThree = new Document(3, "File Three", Path.of("file-three.txt"), "File three is simply quite plain java.");
+        List<Document> documents = List.of(docOne, docTwo, docThree);
+
+        engine.addDocuments(documents);
+        List<SearchResult> results = engine.search("Of java");
+        assertEquals(3, results.size());
+
+        SearchResult first = results.get(0);
+        SearchResult second = results.get(1);
+        SearchResult third = results.get(2);
+
+
+        assertEquals(3, first.getScore());
+        assertSame(docTwo, first.getDocument());
+
+        assertSame(docOne, second.getDocument());
+        assertEquals(2, second.getScore());
+
+        assertSame(docThree, third.getDocument());
+        assertEquals(1, third.getScore());
+    }
+}
