56cd9f7c3d93acf2d3406fabdde616a9ce10cd23
Lia <lia.erisson@mail.mcgill.ca>
2026-07-31 14:36:27 -0400

    Fleshed out SearchResult class. Added to personal notes.

--- src/main/java/io/github/liaerisson/search/SearchResult.java
+++ src/main/java/io/github/liaerisson/search/SearchResult.java
@@ -1,4 +1,17 @@
 package io.github.liaerisson.search;
 
+import io.github.liaerisson.document.Document;
+
 public class SearchResult {
+    private final Document document;
+    private final int score;
+
+    public SearchResult(Document document, int score) {
+        this.document = document;
+        this.score = score;
+    }
+
+    public int getScore() {
+        return score;
+    }
 }
