0c59cb6381f99edd35bf0186e47b953a9000d8be
Lia <lia.erisson@mail.mcgill.ca>
2026-07-31 05:55:10 -0400

    Created tokenizer object to InvertedIndex instantiation. Added internal logic of addDocument.

--- src/main/java/io/github/liaerisson/indexing/InvertedIndex.java
+++ src/main/java/io/github/liaerisson/indexing/InvertedIndex.java
@@ -7,16 +7,36 @@ import java.util.HashMap;
 public class InvertedIndex {
     private HashMap<String, HashMap<Integer, Integer>> terms;
     private HashMap<Integer, Document> documentsById;
+    private Tokenizer tokenizer;
 
     public InvertedIndex() {
         terms = new HashMap<>(); //word, which document IDs have this word
         documentsById = new HashMap<>(); //ID for each document
+        tokenizer = new Tokenizer();
     }
 
     public void addDocument(Document document) {
         int id = document.getId();
         if(documentsById.containsKey(id)) {
             return;
+        }
+
+        documentsById.put(id, document);
+        String[] words = tokenizer.tokenizeWords(document.getContent());
+
+        for(String word: words) {
+            if(!terms.containsKey(word)) {
+                HashMap<Integer, Integer> documentCounts = new HashMap<>();
+                terms.put(word, documentCounts);
+            }
+
+            HashMap<Integer, Integer> documentCounts = terms.get(word);
+            if(documentCounts.get(id) == null) {
+                documentCounts.put(id, 1);
+            } else {
+                documentCounts.put(id, documentCounts.get(id) + 1);
+            }
+            terms.put(word, documentCounts);
         }
     }
 
