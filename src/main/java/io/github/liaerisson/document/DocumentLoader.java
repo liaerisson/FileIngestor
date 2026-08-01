418b6c24ea905255a1e18e1f3bcd14138906c441
Lia <lia.erisson@mail.mcgill.ca>
2026-07-31 20:18:09 -0400

    Feat: Made DocumentLoader class handle directory path input rather than direct insertion of filepath. Updated notes.

--- src/main/java/io/github/liaerisson/document/DocumentLoader.java
+++ src/main/java/io/github/liaerisson/document/DocumentLoader.java
@@ -1,34 +1,15 @@
 package io.github.liaerisson.document;
 import java.io.BufferedReader;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.stream.Stream;
 
 public class DocumentLoader {
-
-    public ArrayList<Path> getFiles() {
-        String path = "/Users/liaer/GitHub/JavaIngestor/src/main/java/io/github/liaerisson/documents";
-        ArrayList<Path> fileSet = new ArrayList<>();
-
-        try(Stream<Path> paths = Files.walk(Paths.get(path))) { //returns a file stream
-            paths
-                    .filter(Files::isRegularFile) //is a file, not a folder or other non-file structure
-                    .filter(file -> file.toString().endsWith(".txt")) //filter to ensure each file ends with .txt
-                    .forEach(fileSet::add);
-
-        } catch (IOException e) {
-            throw new RuntimeException("Count not read documents folder", e);
-        }
-
-        return fileSet;
-    }
-
-    public ArrayList<Document> fileLoader() {
-        ArrayList<Path> files = getFiles();
+    public List<Document> loadDocuments(Path directory) {
+        ArrayList<Path> files = getFiles(directory);
         ArrayList<Document> documents = new ArrayList<>();
 
         for(int i = 0; i < files.size(); i++) {
@@ -39,12 +20,28 @@ public class DocumentLoader {
         return documents;
     }
 
-    public Document makeDocument(Path filePath, int id) {
+    private ArrayList<Path> getFiles(Path directory) {
+        ArrayList<Path> files = new ArrayList<>();
+
+        try(Stream<Path> paths = Files.walk(directory)) { //returns a file stream
+            paths
+                    .filter(Files::isRegularFile) //is a file, not a folder or other non-file structure
+                    .filter(file -> file.toString().endsWith(".txt")) //filter to ensure each file ends with .txt
+                    .sorted()
+                    .forEach(files::add);
+
+        } catch (IOException e) {
+            throw new RuntimeException("Count not read directory: " + directory, e);
+        }
+
+        return files;
+    }
+
+    private Document makeDocument(Path filePath, int id) {
         String fileName = filePath.getFileName().toString(); //Get name
         StringBuilder content = new StringBuilder();
 
         try(BufferedReader reader = Files.newBufferedReader(filePath)) {
-            //System.out.println("This file exists!"); -- Used this to test it works :)
             String line;
 
             while ((line = reader.readLine()) != null) { // This check exists to ensure the reader stops at the end of the file
@@ -52,14 +49,11 @@ public class DocumentLoader {
                 content.append(System.lineSeparator()); //line split
             }
 
-        } catch (FileNotFoundException e) {
-            System.out.println("Count not locate file");
         } catch (IOException e) {
-            System.out.println("Something went wrong");
+            throw new RuntimeException("Count not read file: " + filePath, e);
         }
 
-        Document doc = new Document(id, fileName, filePath, content.toString());
-        return doc;
+        return new Document(id, fileName, filePath, content.toString());
     }
 
 }
